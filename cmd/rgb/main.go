7d4678f7a037ff123a8de2152251c87e3d97d383
Aditya77kushwaha <adityakushwaha2912@gmail.com>
2022-02-08 14:45:06 +0530

    uses nodemon for hot reloading

--- cmd/rgb/main.go
+++ cmd/rgb/main.go
@@ -5,3 +5,6 @@ import "rgb/internal/server"
 func main() {
   server.Start()
 }
+
+//For hot reloading using nodemon, inside root dir run :
+//nodemon --watch './**/*.go' --signal SIGTERM --exec 'go' run cmd/rgb/main.go
