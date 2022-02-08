5eece5d3d8128989fc336a122a6abb700f695d46
Aditya77kushwaha <adityakushwaha2912@gmail.com>
2022-02-08 15:20:59 +0530

    Creates and updates a new user(testuser) for postgresql

--- internal/database/database.go
+++ internal/database/database.go
@@ -8,7 +8,10 @@ func NewDBOptions() *pg.Options {
   return &pg.Options{
     Addr:     "localhost:5432",
     Database: "rgb",
-    User:     "thegreataditya",
-    Password: "Hanuman2912@",
+    User:     "testuser",
+    Password: "Aditya123@",
   }
 }
+
+// # Create a new PostgreSQL user called testuser, allow user to login, but NOT creating databases
+// $ sudo -u postgres createuser --login --pwprompt testuser
